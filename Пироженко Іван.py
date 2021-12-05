f753e593893855477b286a5cbd1f00224c16f4c0
Pyrozhenko Ivan <71495576+pyrozhenko@users.noreply.github.com>
2021-12-05 18:10:52 +0200

    Update Пироженко Іван.py

--- Пироженко Іван.py
+++ Пироженко Іван.py
@@ -1,6 +1,5 @@
-Python 3.9.9 (tags/v3.9.9:ccb0e6a, Nov 15 2021, 18:08:50) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from numpy import *
+"lab #14"
+from numpy import *
 from math import *
 import matplotlib.pyplot as plt
 
